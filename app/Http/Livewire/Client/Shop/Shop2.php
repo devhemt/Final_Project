06880c1829673ed74594ff974a34cf6afa312183
devhemt <96639094+devhemt@users.noreply.github.com>
2023-07-06 21:48:51 +0700

    conflict

---
+++ app/Http/Livewire/Client/Shop/Shop2.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Client\Shop;
+
+use Livewire\Component;
+
+class Shop2 extends Component
+{
+    public function render()
+    {
+        return view('livewire.shop2');
+    }
+}
